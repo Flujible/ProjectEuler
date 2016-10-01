9ea4430dd08671ee501c8cecfbe1e0b52f5a74ba
George Bryant <george.s.bryant@gmail.com>
2016-10-01 18:06:40 +0100

    Added another method to complete the problem without using the fib method too many times 1 problem - Magic number in mkArr function - not sure how to figure out the optimum length of the intial array

--- ProEuler2.js
+++ ProEuler2.js
@@ -1,5 +1,5 @@
 console.log("\n=============================================================================\n");
-
+console.log("New Method");
 //Recursive fibonacci algorithm
 let fib = function fib(n) {
   if(n <= 2) {
@@ -8,6 +8,41 @@ let fib = function fib(n) {
     return fib(n-1) + fib(n-2);
   }
 }
+
+
+let mkArr = function(n) {
+  //Create an array with n values
+  let arr = [];
+  let i = 0;
+  while (i < n) {
+    arr.push(i);
+    i ++;
+  }
+  return arr;
+}
+
+//Make an array and perform Fibonaci algorithm on all elements
+arr = mkArr(35);
+fibArr = arr.map(fib);
+
+//Remove all elements that are larger than 4000000 or that are not even
+fibArrLength = fibArr.length;
+for (let i = fibArrLength; i >= 0 ; i--) {
+  if (fibArr[i] % 2 != 0 || fibArr[i] > 4000000) {
+    fibArr.splice(i, 1);
+  }
+}
+
+//Add all elments of the array together
+function getSum(total, num) {
+    return total + Math.round(num);
+}
+fibArrSum = fibArr.reduce(getSum, 0);
+
+console.log(fibArrSum);
+
+console.log("\n=============================================================================\n");
+console.log("Original Method");
 
 let count = 0;
 let sum = 0;
